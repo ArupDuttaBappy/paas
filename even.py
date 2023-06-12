37decd648b47f2c419fcce6c3d60379bd9252319
Arup Dutta Bappy <ad.bappy10@gmail.com>
2023-06-13 00:37:52 +0600

    Update even.py

--- even.py
+++ even.py
@@ -1,25 +1,14 @@
 from flask import Flask
-
-
-# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
-# called `app` in `main.py`.
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello():
-    """Return a friendly HTTP greeting."""
-    
-    a = []
-
-    for e in range(1,12,1):
-        if (e%2) == 0:
-            a.append(e)
+    arr = []
+    for i in range(2,22,1):
+        if (i%2) == 0:
+            arr.append(i)
     return a
 
-
 if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. You
-    # can configure startup instructions by adding `entrypoint` to app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
+    
